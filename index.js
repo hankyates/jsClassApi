3e1e938b425a65cc495fafa98fdea4c3631743b0
Hank Yates <hankyates@gmail.com>
2014-04-18 20:50:17 +0000

    simple api for class

---
+++ index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+
+var enableCORS = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    // intercept OPTIONS method
+    if (req.method === 'OPTIONS') {
+      res.send(200);
+    } else {
+      next();
+    }
+}
+
+var content = {
+  'home': '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>',
+  'articles': '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>',
+  'portfolio': '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>'
+}
+
+app.get('/content', function(req, res){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.json(content);
+});
+
+app.get('/content/home', function(req, res){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.json(content['home']);
+});
+
+app.get('/content/articles', function(req, res){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.json(content['articles']);
+});
+
+app.get('/content/portfolio', function(req, res){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.json(content['portfolio']);
+});
+
+app.listen(3000);
